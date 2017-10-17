f8f4afa1f30226fa6989b2d1c42a605c33116d66
Antonio Montillo <antonio.montillo@onedaygroup.it>
2017-10-17 18:26:28 +0200

    add some happy

--- htdocs/app/config.php
+++ htdocs/app/config.php
@@ -1,7 +1,7 @@
 <?php
 //Config Database
-define('DB_HOST', 'localhost');
-define('DB_USER', 'anto');
-define('DB_PASS', 'ciao');
-define('DB_NAME', 'cosa');
+define('DB_HOST', 'mariadbzoo.internal');
+define('DB_USER', 'fotostation_usr');
+define('DB_PASS', 'F065pV31XUyCGVLc');
+define('DB_NAME', 'fotostation2');
 define('ENCRYPTION_KEY', 'c2d1ef8b74bcddd');
